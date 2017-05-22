f96350eff30149b46412396e77ab68a3f66b587b
Administrator <admin@tecman.ru>
2017-05-22 10:56:26 +0300

    initial refactoring

--- src/main/java/aqua/galacticplus/GalacticPlus.java
+++ src/main/java/aqua/galacticplus/GalacticPlus.java
@@ -1,12 +1,12 @@
-package com.example.examplemod;
+package aqua.galacticplus;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = GalacticPlus.MODID, version = GalacticPlus.VERSION)
+public class GalacticPlus
 {
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
